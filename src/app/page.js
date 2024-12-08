efcbc24ef2005a98a951bcc550e43f3116e6529a
Rishabh Ranjan <rk.remo12345@gmail.com>
2024-12-08 16:43:04 +0530

    fix img

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,26 @@
+"use client"
 import { Box } from "@radix-ui/themes";
 import { Container } from "postcss";
+import Instacard from "./components/instacard";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      const response = await fetch('https://graph.instagram.com/v12.0/Rainaxsinhaa/media?fields=media_url&access_token=YOUR_ACCESS_TOKEN');
+      const data = await response.json();
+      setPhotos(data.data);
+    };
+
+    fetchPhotos();
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Box style={{ background: "var(--gray-a2)", borderRadius: "var(--radius-3)" }}>
-          <Container size="1">
-              <Box py="9" />
-          </Container>
-      </Box>  
-
+      {photos.map(photo => (
+        <Instacard links={photo.media_url} likes={"goodle"} key={photo.id}/>
+      ))}
     </div>
   );
 }
