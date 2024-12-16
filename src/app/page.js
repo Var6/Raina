db4c5e7eac2d39a14a73319e3d57926dcdb5b58c
Rishabh Ranjan <rk.remo12345@gmail.com>
2024-12-16 23:39:40 +0530

    correction5

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,14 @@
-// "use client"
-// import Instacard from "./components/instacard";
-// import { useEffect, useState } from "react";
+
 import Image from "next/image"
 import React from "react";
+// import { StarsBackground } from "./components/backgroundstar";
+// import { ShootingStars } from "./components/Star";
 
 export default function Home() {
-  // const [photos, setPhotos] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchPhotos = async () => {
-  //     const response = await fetch('https://graph.instagram.com/v12.0/Rainaxsinhaa/media?fields=media_url&access_token=YOUR_ACCESS_TOKEN');
-  //     const data = await response.json();
-  //     setPhotos(data.data);
-  //   };
-
-  //   fetchPhotos();
-  // }, []);
-  return (
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-            <div className="grid gap-4">
+    return (
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 bg-black">
+            <div className="grid gap-4 z-50 ">
                 <div>
                     <Image width={200} height={100} className="h-auto max-w-full rounded-lg" src="/Photos/2.jpeg" alt=""/>
                 </div>
@@ -29,17 +19,19 @@ export default function Home() {
                     <Image width={100} height={100} className="h-auto max-w-full rounded-lg" src="/Photos/2.jpeg" alt=""/>
                 </div>
             </div>
-            <div className="grid gap-4">
+            <div className="grid gap-4 z-50">
                 <div>
-                    <Image width={100} height={100} className="h-auto max-w-full rounded-lg" src="/Photos/1.jpg" alt=""/>
+                    <Image width={100} height={100} className="h-auto max-w-full rounded-lg" src="/Photos/1.jpg" alt="asdf"/>
                 </div>
                 <div>
-                    <Image width={100} height={100} className="h-auto max-w-full rounded-lg" src="/Photos/2.jpeg" alt=""/>
+                    <Image width={100} height={100} className="h-auto max-w-full rounded-lg" src="/Photos/2.jpeg" alt="adsf"/>
                 </div>
                 <div>
-                    <Image width={100} height={100} className="h-auto max-w-full rounded-lg" src="/Photos/1.jpg" alt=""/>
+                    <Image width={100} height={100} className="h-auto max-w-full rounded-lg" src="/Photos/1.jpg" alt="adf"/>
                 </div>
             </div>
+            {/* <ShootingStars minDelay={1000} maxDelay={2600} starWidth={13} maxSpeed={20} minSpeed={10}/>
+        <StarsBackground starDensity={0.0015} maxTwinkleSpeed={3}/> */}
         </div>
 
   );
